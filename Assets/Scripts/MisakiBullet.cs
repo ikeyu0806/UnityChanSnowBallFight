f16b38aa6852de4a20271046585cc16762ec74c6
ikeyu0806 <ikeyu0806@gmail.com>
2022-01-02 16:26:42 +0900

    add Misaki Model

--- Assets/Scripts/MisakiBullet.cs
+++ Assets/Scripts/MisakiBullet.cs
@@ -4,15 +4,68 @@ using UnityEngine;
 
 public class MisakiBullet : MonoBehaviour
 {
+    float shotSpeed = 10f;
+    int attackDamage = 10;
+
+    GameObject Player;
+    GameObject MisakiModel;
+    enum Direction { Up, Down, Right, Left };
+    Direction shotDirection;
+    Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        MisakiModel = GameObject.Find("EnemyMisaki");
+        animator = GetComponent<Animator>();
+        switch (MisakiModel.GetComponent<BossEnemy>().bossDirection)
+        {
+            case BossEnemy.Direction.Up:
+                shotDirection = Direction.Up;
+                break;
+            case BossEnemy.Direction.Down:
+                shotDirection = Direction.Down;
+                break;
+            case BossEnemy.Direction.Right:
+                shotDirection = Direction.Right;
+                break;
+            case BossEnemy.Direction.Left:
+                shotDirection = Direction.Left;
+                break;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (shotDirection)
+        {
+            case Direction.Up:
+                transform.position += new Vector3(0, 0, shotSpeed) * Time.deltaTime;
+                break;
+            case Direction.Down:
+                transform.position += new Vector3(0, 0, -shotSpeed) * Time.deltaTime;
+                break;
+            case Direction.Right:
+                transform.position += new Vector3(shotSpeed, 0, 0) * Time.deltaTime;
+                break;
+            case Direction.Left:
+                transform.position += new Vector3(-shotSpeed, 0, 0) * Time.deltaTime;
+                break;
+        }
+    }
+
+    public void DamageEnemy(Collision enemy)
+    {
+        enemy.gameObject.GetComponent<PlayerController>().TakeHit(attackDamage);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            DamageEnemy(collision);
+            Destroy(gameObject);
+        }
     }
 }
